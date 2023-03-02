da63d91c52d94687c5c78754cb771e6323dbf110
Shibily-kms <kmsshibily@gmail.com>
2023-03-02 08:55:59 +0530

    user sign up completed

--- src/components/user/Sign/SignUp.jsx
+++ src/components/user/Sign/SignUp.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.scss'
 import image from '../../../assets/images/log1.png'
+import { toast } from 'react-toastify'
+import axios from '../../../config/axios'
 
 function SignUp({ setSignUp }) {
+  const [form, setForm] = useState({ fullName: "", email: "", password: "", cpassword: "", mobile: "" })
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (form.fullName === "" || form.email === "" || form.password === "" || form.cpassword === "" || form.mobile === "") {
+      toast.error('Fill all Inputs')
+    } else if (form.password !== form.cpassword) {
+      toast.error('Password not match')
+    } else if (form.mobile.length !== 10) {
+      toast.error('Mobile number not ready')
+    } else {
+      axios.post('/sign-up', form).then((response) => {
+        toast.success(response.data.message+', Login Now')
+        setSignUp(false)
+      }).catch((error) => {
+        toast.error(error.response.data.message)
+      })
+    }
+  }
   return (
     <div className='sign'>
       <div className="boader">
@@ -16,14 +42,14 @@ function SignUp({ setSignUp }) {
           <div className="right">
             <div className="form">
               <h3>Let’s get started!</h3>
-              <form action="">
-                <input type="text" name='fullName' placeholder='Full Name' />
-                <input type="email" name='emailId' placeholder='Email' />
-                <input type="password" name='password' placeholder='Password' />
-                <input type="password" name='cpassword' placeholder='Confirm Password' />
-                <input type="number" name='mobile' placeholder='+91 MobileNumber' />
+              <form action="" onSubmit={handleSubmit}>
+                <input type="text" name='fullName' placeholder='Full Name' onChange={handleChange} />
+                <input type="email" name='email' placeholder='Email' onChange={handleChange} />
+                <input type="password" name='password' placeholder='Password' onChange={handleChange} />
+                <input type="password" name='cpassword' placeholder='Confirm Password' onChange={handleChange} />
+                <input type="number" name='mobile' placeholder='MobileNumber' onChange={handleChange} />
                 <div className="checkbox">
-                  <input type="checkbox" id='check' name='check' />
+                  <input type="checkbox" id='check' name='check' required />
                   <label htmlFor="check">I Agree to terms & conditions</label>
                 </div>
                 <button>Register</button>
